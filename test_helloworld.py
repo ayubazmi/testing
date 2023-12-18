966a10b84c06ac6b73dff1b0c2a00356df722c7a
ayubazmi <119778643+ayubazmi@users.noreply.github.com>
2023-12-18 14:05:20 +0530

    test case update

--- test_helloworld.py
+++ test_helloworld.py
@@ -6,8 +6,12 @@ class TestHelloWorld(unittest.TestCase):
 
     def test_say_hello(self):
         result = say_hello()
-        self.assertEqual(result, "Hello, World!")
+        expected_result = "Hello, World!"
 
+        if result == expected_result:
+            print("Test case pass.")
+        else:
+            print(f"Test case fails. Expected '{expected_result}', but got '{result}'.")
 
 if __name__ == '__main__':
     unittest.main()
