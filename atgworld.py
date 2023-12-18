89f3256c5315cd1c7767115b38ea61903e3bfe3d
ayubazmi <119778643+ayubazmi@users.noreply.github.com>
2023-12-18 16:45:19 +0530

    atg.world test case file

--- atgworld.py
+++ atgworld.py
@@ -1,39 +1,30 @@
 import unittest
 import requests
 
-
 class TestWebsiteConnection(unittest.TestCase):
 
     def test_website_connection(self):
-        # Define the website URL
-        website_url = "https://atg.world"
 
-        # Log step: Start of the test
+        website_url = "https://atg.world"
         print("Starting the website connection test...")
 
-        # Try to make a request to the website
         try:
-            # Log step: Sending request to the website
             print(f"Sending a GET request to {website_url}")
             response = requests.get(website_url)
 
-            # Log step: Checking response status code
             print(f"Response status code: {response.status_code}")
 
-            response.raise_for_status()  # Raise HTTPError for bad responses
-
-            # Log step: Website loaded successfully
+            response.raise_for_status()
             print(f"Successfully connected to {website_url}")
             print(f"Test case pass")
 
-            self.assertEqual(response.status_code, 200)  # Check if the response status is 200 (OK)
+            self.assertEqual(response.status_code, 200)
 
         except requests.exceptions.RequestException as e:
-            # Log step: Exception occurred, website didn't load
             print(f"Failed to connect to {website_url}. Error: {e}")
             self.fail(f"Failed to connect to {website_url}. Error: {e}")
 
-
 if __name__ == '__main__':
     unittest.main()
+
 
