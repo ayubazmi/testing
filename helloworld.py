dc99cf4e126413ed228534b530d232c2148fc63b
ayubazmi <119778643+ayubazmi@users.noreply.github.com>
2023-12-18 13:51:47 +0530

    files uploaded

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+# helloworld.py
+
+def say_hello():
+    return "Hello, World!"
+
---
+++ test_helloworld.py
@@ -0,0 +1,14 @@
+# test_helloworld.py
+import unittest
+from helloworld import say_hello
+
+class TestHelloWorld(unittest.TestCase):
+
+    def test_say_hello(self):
+        result = say_hello()
+        self.assertEqual(result, "Hello, World!")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
